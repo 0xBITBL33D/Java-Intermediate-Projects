a4583a55a16e6d3deddd450b6e1538b155ba2c00
Sho Nakamura <0xbitbleed@gmail.com>
2025-07-19 07:35:18 -0400

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -1,9 +1,13 @@
 class Bank{
 
-    String Menu = "Welcome to Bitbl33d's Bank\n"  +
-                  "How may we help you?\n\n"    +
-                  "[0]";
+    String Menu = "Welcome to Bitbl33d's Bank\n"    +
+                  "How may we help you?\n\n"        +
 
+
+                  "[0] - Bank Balance\n"            +
+                  "[1] - Withdraw\n"                +
+                  "[2] - Make a New Account\n"      +
+                  "[3] - ";
     public static void main(String[] args) {
         
     }
