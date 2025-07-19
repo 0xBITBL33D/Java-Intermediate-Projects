3950791659ed5191ef599bede68d8c1c19ab8af7
Sho Nakamura <0xbitbleed@gmail.com>
2025-07-19 06:59:41 -0400

    Create Bank.java

---
+++ Bank.java
@@ -0,0 +1,10 @@
+class Bank{
+
+    String Menu = "Welcome to Bitbl33d's Bank\n"  +
+                  "How may we help you?\n\n"    +
+                  "[0]";
+
+    public static void main(String[] args) {
+        
+    }
+}
