f05da1c01e140ecfce52a0e2fe5410d2205492e7
Sho Nakamura <0xbitbleed@gmail.com>
2025-07-20 00:24:32 -0400

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -7,8 +7,11 @@ class Bank{
                   "[0] - Bank Balance\n"            +
                   "[1] - Withdraw\n"                +
                   "[2] - Make a New Account\n"      +
-                  "[3] - ";
+                  "[3] - Draft a Complaint";
     public static void main(String[] args) {
-        
+        Bank bank = new Bank();
+
+        System.out.print(bank.Menu);
+
     }
 }
