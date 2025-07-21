8114a033b4513cb7b550aa8e5ce830506afd13eb
Sho Nakamura <0xbitbleed@gmail.com>
2025-07-21 06:02:13 -0400

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -1,3 +1,17 @@
+/*
+    Create a BankAccount class that lets users:
+
+    Set an initial balance via constructor
+
+    Deposit and withdraw money
+
+    Check the current balance
+
+    Prevent balance from going negative (encapsulation logic)
+ */
+
+import java.util.Scanner;
+
 public class Bank{
     
     public String menu = "Welcome to Bitbl33d's Bank!\n\n"  +
@@ -7,10 +21,26 @@ public class Bank{
                          "[3] - Delete an Account"          ;
 
 
+    //DEFAULT BALANCE
+    private double bankBalance = 0;
+
+    void getBalance(){
+        System.out.print(bankBalance);
+    }
+
+    //MENU LOOP
+    public static void MenuLoop() {
+        
+    }
+
     public static void main(String[] args) {
         Bank Bank = new Bank();
 
         System.out.print(Bank.menu);
+
+        Scanner sc = new Scanner(System.in);
+
+        
 
         
     }
